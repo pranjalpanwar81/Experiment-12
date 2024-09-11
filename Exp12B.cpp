b5481c2d9fb2c62003256a94f97f0a17729e66a2
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2024-09-11 08:50:39 +0530

    Create Exp12B.cpp

---
+++ Exp12B.cpp
@@ -0,0 +1,28 @@
+// NAME - PranjaL Panwar 
+// PRN - 23070123164
+// EXPERIMENT - 12(B) 
+
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater.";
+        }
+        else 
+        {
+            cout<<d<<" is greater.";
+        }
+    }
+};
+int main()
+{
+    Num n1(4,3);
+} 
+
+// Output 
+// 4 is greater. 
